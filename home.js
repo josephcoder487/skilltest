88f73b484cdc635bde05258e1d7f9af8ac945493
josephcoder487 <joseph.noah487@gmail.com>
2026-05-15 23:38:19 +0530

    3.6v added MTQ questions tab and javascript functions

---
+++ home.js
@@ -0,0 +1,26 @@
+const test1 = document.getElementById("test1");
+const firstpage = document.querySelector(".firstpage");
+const fullstack = document.querySelector(".fullstack");
+const back1 = document.querySelector(".back1");
+test1.addEventListener("click", () => {
+    firstpage.style.display = "none";
+    fullstack.style.display = "block";
+})
+back1.addEventListener("click", () => {
+    firstpage.style.display = "block";
+    fullstack.style.display = "none";
+})
+
+// timer ======= ithu apram ahh pathukalam
+const timer = document.getElementById("timer");
+
+// let timeLeft = 60; // 60 seconds
+// const countdown = setInterval(() => {
+//     if (timeLeft <= 0) {
+//         clearInterval(countdown);
+//         timer.textContent = "Time's up!";
+//     } else {
+//         timeLeft--;
+//         timer.textContent = `Time left: ${timeLeft} seconds`;
+//     }
+// }, 1000);
